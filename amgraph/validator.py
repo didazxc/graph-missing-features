f614dc83579594617caa7cdb9fa6649e873ef2ef
j-zhangxiaochuan-jk <834472975@qq.com>
2023-03-22 12:21:37 +0800

    use induced subgraph to class

--- amgraph/validator.py
+++ amgraph/validator.py
@@ -10,6 +10,7 @@ import torch
 from sklearn.model_selection import KFold
 from torch import nn
 import multiprocessing
+from torch_geometric.utils import subgraph
 
 
 logger = logging.getLogger('amgraph.apa')
@@ -30,7 +31,7 @@ class EstimationValidator:
         self.max_num_iter = max_num_iter
         self.min_num_iter = min_num_iter
         self.all_metrics, self.all_ks = self.metric_and_ks(dataset_name)
-        self.metric, self.k = self.all_metrics[0], self.all_ks[k_index]
+        self.metric, self.k = self.all_metrics[k_index], self.all_ks[k_index]
         self.early_stop = early_stop
 
     @staticmethod
@@ -164,16 +165,17 @@ class EstimationValidator:
 
 class ClassificationValidator:
 
-    def __init__(self, scores: Scores, dataset_name:str, edges, x_all, y_all, test_nodes, num_classes, seed, epoches, seed_idx) -> None:
+    def __init__(self, scores: Scores, dataset_name:str, edges, y_all, test_nodes, num_attrs, num_classes, epoches, seed, seed_idx, k_index=0) -> None:
         self.scores = scores
         self.dataset_name = dataset_name
+
         self.edges = edges
-        self.x_all: torch.Tensor = x_all
         self.y_all = y_all
         self.test_nodes = test_nodes
-        self.num_nodes = x_all.size(0)
+
         self.num_classes = num_classes
-        self.num_attrs = x_all.size(1)
+        self.num_attrs = num_attrs
+
         self.seed = seed
         self.seed_idx = seed_idx
         self.epoches = epoches
@@ -183,6 +185,7 @@ class ClassificationValidator:
         else:
             self.ks = [3, 5, 10] if dataset_name.startswith("steam") else [10, 20, 50]
             self.metrics = ["Recall", "nDCG"]
+        self.metric, self.k = self.metrics[k_index], self.ks[k_index]
 
     @staticmethod
     def train(model, loss_fn, optimizer, epoches, x, edges, y, trn_nodes, val_nodes) -> torch.Tensor:
@@ -205,19 +208,17 @@ class ClassificationValidator:
         return acc
 
     def calc_acc(self, x_hat, conv="GCN"):
-        x = self.x_all.clone().detach()
-        x[self.test_nodes] = x_hat[self.test_nodes]
         acc_list = []
         splits=KFold(n_splits=5,shuffle=True,random_state=self.seed)
         logger.info(f"start model {conv}")
-        for trn_nodes, val_nodes in splits.split(range(self.num_nodes)):
+        for trn_nodes, val_nodes in splits.split(self.test_nodes):
             if conv=="GCN":
-                model = GNN(x_hat.size(1), self.num_classes, num_layers=2, hidden_size=256)
+                model = GNN(self.num_attrs, self.num_classes, num_layers=2, hidden_size=256)
             else:
-                model = MLP(x_hat.size(1), self.num_classes, num_layers=2, hidden_size=256)
+                model = MLP(self.num_attrs, self.num_classes, num_layers=2, hidden_size=256)
             loss_fn = nn.CrossEntropyLoss()
             optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-            acc = self.train(model, loss_fn, optimizer, self.epoches, x, self.edges, self.y_all, trn_nodes, val_nodes)
+            acc = self.train(model, loss_fn, optimizer, self.epoches, x_hat, self.edges, self.y_all, trn_nodes, val_nodes)
             logger.info(f"model {conv} acc {acc}")
             acc_list.append(acc)
         acc = np.mean(acc_list)
@@ -234,27 +235,28 @@ class ClassificationValidator:
         self.scores.add_score(new_row, new_col, value)
         self.scores.print()
 
-    def validate_from_scores(self, apa_fn, est_scores: Scores):
+    def validate_from_scores(self, apa_fn, est_scores: Scores, val_only_once):
         algo_name = apa_fn.__name__
-        for metric in self.metrics:
-            print(self.ks)
-            for k in self.ks:
+        convs = ["GCN"]  # ["GCN", "MLP"]
+        metrics, ks = ([self.metric], [self.k]) if val_only_once else (self.metrics, self.ks)
+        for metric in metrics:
+            for k in ks:
                 row = f"{metric}_{algo_name}"
                 col = f"{self.dataset_name}@{k}_params"
 
-                if not self._is_executed(row, col, "GCN") or not self._is_executed(row, col, "MLP"):
+                if any([not self._is_executed(row, col, conv) for conv in convs]):
                     params = est_scores.get_value(row, col)[self.seed_idx]
                     logger.info(f"{algo_name}, {row}, {col}, {params}")
                     x_hat=apa_fn(**params)
 
-                    for conv in ["GCN"]:
+                    for conv in convs:
                         if not self._is_executed(row, col, conv):
                             acc = self.calc_acc(x_hat, conv)
                             self._add_score(row, col, conv, acc)
         self.scores.print()
 
     @staticmethod
-    def run(file_name="class_all_umtp30", est_scores_file_name="all_umtp30", dataset_names = ['cora', 'citeseer', 'computers', 'photo', 'steam', 'pubmed', 'cs', 'arxiv'], run_algos=['fp', 'pr', 'ppr', 'mtp', 'umtp', 'umtp2']):
+    def run(file_name="class_all_umtp30", est_scores_file_name="all_umtp30", dataset_names = ['cora', 'citeseer', 'computers', 'photo', 'steam', 'pubmed', 'cs', 'arxiv'], run_algos=['fp', 'pr', 'ppr', 'mtp', 'umtp', 'umtp2'], val_only_once=True, k_index=0):
         est_scores = Scores(file_name=est_scores_file_name)
         est_scores.load()
         scores = Scores(file_name=file_name)
@@ -270,7 +272,8 @@ class ClassificationValidator:
             for dataset_name in dataset_names:
                 edge_index, x_all, y_all, trn_nodes, val_nodes, test_nodes, num_classes = d.load_data(dataset_name, split=(0.4, 0.1, 0.5), seed=seed)
                 apa = APA(x_all, edge_index, trn_nodes, d.is_binary(dataset_name))
-                c = ClassificationValidator(scores, dataset_name=dataset_name, edges=edge_index, x_all=x_all, y_all=y_all, test_nodes=test_nodes, num_classes=num_classes, seed=seed, epoches=epoches, seed_idx=seed)
+                induced_edge_index, _ = subgraph(test_nodes, edge_index)
+                c = ClassificationValidator(scores, dataset_name=dataset_name, edges=induced_edge_index, y_all=y_all, test_nodes=test_nodes, num_attrs=x_all.size(1), num_classes=num_classes, epoches=epoches, seed=seed, seed_idx=seed, k_index=k_index)
                 algos = {
                         'fp':apa.fp,
                         'pr':apa.pr,
@@ -280,13 +283,19 @@ class ClassificationValidator:
                         'umtp2':apa.umtp2
                     }
                 for algo_name in run_algos:
-                    c.validate_from_scores(algos[algo_name], est_scores)
+                    c.validate_from_scores(algos[algo_name], est_scores, val_only_once)
                 
         scores.print()
         print("end")
 
 
 def main():
-    EstimationValidator.multi_run("combine_k50_iter_eq_30", max_num_iter=30, early_stop=False, k_index= -1)
-    # ClassificationValidator.run(dataset_names=['citeseer'], run_algos=['umtp'])
+    # EstimationValidator.multi_run("combine_all_le30", max_num_iter=30, early_stop=True, only_val_once= False)
+    # EstimationValidator.multi_run("combine_all_eq30", max_num_iter=30, early_stop=False, only_val_once= False)
+    # EstimationValidator.multi_run("combine_k10_le30", max_num_iter=30, early_stop=True)
+    # EstimationValidator.multi_run("combine_k10_eq30", max_num_iter=30, early_stop=False)
+    # EstimationValidator.multi_run("combine_k50_le30", max_num_iter=30, early_stop=True, k_index= -1)
+    # EstimationValidator.multi_run("combine_k50_eq30", max_num_iter=30, early_stop=False, k_index= -1)
+    # ClassificationValidator.run(file_name="class_k10_eq30_test", est_scores_file_name="combine_k10_eq30")
+    ClassificationValidator.run(file_name="class_all_le30", est_scores_file_name="combine_all_le30", val_only_once=False)
 
